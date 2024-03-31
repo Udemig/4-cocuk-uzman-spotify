757571a60afcab93279e1c159cdf5ac42635b5b0
omerglck <omergolcek10@gmail.com>
2024-03-31 16:01:09 +0300

    ekrana cartları renderlama ve müziği oynatma

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 import { getPopuler } from "./scripts/api.js";
-import { renderCards } from "./scripts/ui.js";
+import { renderCards, renderPlaying } from "./scripts/ui.js";
 
 const list = document.querySelector("#list");
 // sayfa yüklenmesini izler
 document.addEventListener("DOMContentLoaded", () => {
   // apiden verileri alır
   getPopuler().then((data) => renderCards(data));
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.id === "play-btn") {
+    // kartı al
+    const card = e.target.closest(".card");
+
+    renderPlaying(card.dataset);
+  }
 });
--- scripts/ui.js
+++ scripts/ui.js
@@ -1,11 +1,58 @@
+const list = document.querySelector("#list");
+console.log(list);
+
+// arayüzü dinamik yapmak için kullandık
 export const renderCards = (songs) => {
   songs.forEach((song) => {
-    console.log(song);
     // * 1.adım: div oluştur.
     const div = document.createElement("div");
+    // * 2.adım: carda müziğin bilgilerini ekle
     div.dataset.url = song.hub.actions?.pop().uri;
     div.dataset.title = song.title;
     div.dataset.photo = song.images?.coverart;
-    console.log(div);
+
+    // * 3.adım: class tanımla
+    div.className = "card";
+    // * 4.adım: içeriğini dinamik yap
+    div.innerHTML = `
+      <figure>
+        <img
+          src="${song.images?.coverart}"
+          alt=""
+        />
+        <div class="play">
+          <i class="bi bi-play-fill" id="play-btn"></i>
+        </div>
+      </figure>
+      <h4>${song.subtitle}</h4>
+      <p>${song.title}</p>
+    
+    `;
+    // * 5.adım: htmle gönder
+    list.appendChild(div);
   });
+};
+
+const playingInfo = document.querySelector(".playing");
+
+// müziğin bilgilerini bas
+export const renderPlaying = (data) => {
+  console.log(data);
+  playingInfo.innerHTML = `
+  <div class="info">
+    <img
+      src="${data.photo}"
+      alt=""
+    />
+    <div>
+      <p>Şu an Oynatılıyor</p>
+      <h3>${data.title}</h3>
+    </div>
+  </div>
+  <audio
+    src="${data.url}"
+    controls
+  ></audio>
+  
+  `;
 };
